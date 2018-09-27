f4401b3b19bc3e6f1f69103f25f9cc632a823e64
Oskari-Tuormaa <oskaritu@gmail.com>
2018-09-27 18:22:47 +0200

    Added QTree functionality

--- Code/main/classes/qtree.js
+++ Code/main/classes/qtree.js
@@ -1,12 +1,98 @@
 class QTree {
-	constructor(x, y, w, h) {
+	constructor(x, y, w, h, limit) {
 		this.particles = [];
 		this.bounds = new Rectangle(x, y, w, h);
+		this.limit = limit;
+		this.children = [];
+		this.isSplit = false;
 	}
 
 	// Add particle to QTree
 	addParticle(p) {
-		this.particles.push(p);
+
+		// If particle is not within QTree bounds, ignore particle
+		if (!this.bounds.contains(p.pos.x, p.pos.y)) {
+			return;
+		}
+
+		// If QTree is split, add particle to all children
+		if (this.isSplit) {
+			this.addParticleChildren(p);
+			return;
+		}
+
+		// If amount of particles + 1 doesn't excede limit, add particle
+		if (this.particles.length + 1 < this.limit) {
+			this.particles.push(p);
+			return;
+
+		}
+
+		// If particle is within bounds, QTree isn't split and particles + 1 excedes
+		// limit, split the QTree and add particle to children.
+		this.split();
+		this.addParticleChildren(p);
+	}
+
+	// Split QTree into 4 children
+	split() {
+		children[0] = new QTree(this.bounds.x - this.bounds.x / 4,
+			this.bounds.y - this.bounds.y / 4,
+			this.bounds.w / 2, this.bounds.h / 2,
+			this.limit);
+
+		children[1] = new QTree(this.bounds.x + this.bounds.x / 4,
+			this.bounds.y - this.bounds.y / 4,
+			this.bounds.w / 2, this.bounds.h / 2,
+			this.limit);
+
+		children[2] = new QTree(this.bounds.x - this.bounds.x / 4,
+			this.bounds.y + this.bounds.y / 4,
+			this.bounds.w / 2, this.bounds.h / 2,
+			this.limit);
+
+		children[3] = new QTree(this.bounds.x + this.bounds.x / 4,
+			this.bounds.y + this.bounds.y / 4,
+			this.bounds.w / 2, this.bounds.h / 2,
+			this.limit);
+
+		this.isSplit = true;
+
+		// Feed current particles to children and delete own particles
+		for (var i = 0; i < this.particles.length; i++) {
+			for (var j = 0; j < this.children.length; j++) {
+				this.children[j].addParticle(particles[i]);
+			}
+		}
+
+		this.particles = [];
+	}
+
+	// Add particle to children
+	addParticleChildren(p) {
+		for (var i = 0; i < this.children.length; i++) {
+			this.children[i].addParticle(p);
+		}
+	}
+
+	// Fetch all particles of child-QTrees that intersect rect
+	fetch(rect) {
+		var temp = [];
+
+		// If rect doesn't intersect bounds, return an empty array
+		if (!this.bounds.intersects(rect)) {
+			return temp;
+		}
+
+		// If QTree is split, fetch particles from children and add those to temp
+		if (this.isSplit) {
+			for (var i = 0; i < this.children.length; i++) {
+				temp = temp.concat(this.children[i].fetch(rect));
+			}
+		} else {
+			temp.concat(particles);
+		}
+		return temp;
 	}
 }
 
